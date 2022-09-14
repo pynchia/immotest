c8c37fcff990cc42b9ed67edf3b3ef674bc5302f
pynchia <pynchia@gmail.com>
2022-09-14 15:16:38 +0200

    add artists EPs

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import items
+from app.api.endpoints import artists
 
 api_router = APIRouter()
 
 
-api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(artists.router, prefix="/artists", tags=["artists"])
